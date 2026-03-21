f19d9faf58847ae7b30b467b620aaa72655d36ed
Ansh Angural <146299223+AnshAngural@users.noreply.github.com>
2026-03-21 19:02:30 -0400

    error solved

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bookings = require("../controllers/bookings");
-const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedIn = require("../middleware/isloggedIn");
 
 router.post("/:id/book", isLoggedIn, bookings.newBooking);
 
